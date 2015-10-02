b955d211c4aa1d4df6ddfdb7da94d15efaeffad5
Kenny Grage <kennygrage@gmail.com>
2015-10-02 15:22:48 -0700

    add ReviewsFactory.js

---
+++ services/ReviewsFactory.js
@@ -0,0 +1,9 @@
+ReviewsFactory.factory('ReviewsFactory', function ReviewsFactory() {
+  var factory = {};
+  factory.reviews = [];
+  factory.addReview = function() {
+    factory.reviews.push({name: factory.reviewName, id: factory.reviews.length + 1, reviewContent: factory.reviewContent });
+    factory.reviewName = null;
+    factory.reviewContent = null;
+  }
+});
