e28577d95795b36e36580f2d421a4cdb93904719
Kenny Grage <kennygrage@gmail.com>
2015-10-02 19:08:10 -0700

    can't get link to work

--- app.js
+++ app.js
@@ -26,6 +26,24 @@ nicksFlix.config(function($stateProvider, $urlRouterProvider) {
         controller: 'ReviewsCtrl'
       }
     }
-  }); //end $stateProvider 'login'
+  }); //end $stateProvider 'second-div'
+
+  $stateProvider.state('second-div2', {
+    url: "/:reviewId",
+    views: {
+      'header': {
+        templateUrl: "partials/header.html",
+        controller: 'HeadersCtrl'
+      },
+      'body': {
+        templateUrl: "partials/second-div",
+        controller: 'ReviewsCtrl'
+      },
+      'current_review': {
+        templateUrl: "partials/second-div.specific_review.html",
+        controller: 'ReviewsCtrl'
+      }
+    }
+  }); //end $stateProvider 'second-div.specific_review'
 
 }); //end nicksFlix.config
